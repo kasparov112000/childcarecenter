286764ef658af86e92f7fa65978c9655ce00b980
Renato Nunez <212marines@gmail.com>
2018-11-29 13:54:43 -0800

    latest

---
+++ src/app/camara/camara.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { CamaraComponent } from './camara.component';
+
+describe('CamaraComponent', () => {
+  let component: CamaraComponent;
+  let fixture: ComponentFixture<CamaraComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CamaraComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CamaraComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
